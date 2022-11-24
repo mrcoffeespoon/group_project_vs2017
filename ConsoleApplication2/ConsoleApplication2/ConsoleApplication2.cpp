03d5fa906bb2d2d64a3cdae1844455622692d676
Davidlai01 <106083462+Davidlai01@users.noreply.github.com>
2022-11-24 18:32:25 +0800

    Update ConsoleApplication2.cpp

--- ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -101,36 +101,37 @@ protected:
 	}
 
 	void Show_Reservation_Record() {
-		cout<<"*** Reservation Record ***\n"<<
-		"Display Which Room (A - C):"<<endl;
+		cout << "*** Reservation Record ***\n" <<
+			"Display Which Room (A - C):" << endl;
 		char c;				//choice
-		cin>>c;
+		cin >> c;
 		int x;
-		if(c=='A')x=1;
-		else if(c=='B')x=2;
-		else if(c=='C')x=3;
+		if (c == 'A')x = 1;
+		else if (c == 'B')x = 2;
+		else if (c == 'C')x = 3;
+		else return;
 		
-		cout<<setw(6)<<left<<' ';
-		int i;
-		for(i = 1;i<=7;i++){
-			cout<<setw(6)<<date[i];
-		}
-		cout<<endl;
-		
-		for(i = 0;i<13;i++){
-			cout<<setw(6);
-			print_time(i+9);
-			
-			for(int j=1; j<=7;j++)
-			{	
-				if(timetable[x][j][i] >0)
-					cout<<"  x    ";
-				else cout<<"      ";
-			}
-			cout<<endl;
-		}
-	}//R.2
 
+		cout << setw(6) << left << ' ';
+		int i;
+		for (i = 1; i <= 7; i++) {
+			cout<< date[i]<<"  ";
+		}
+		cout << endl;
+
+		for (i = 0; i < 13; i++) {
+			print_time(i + 9);
+			cout << "  ";
+			for (int j = 1; j <= 7; j++)
+			{
+				if (timetable[x][j][i] > 0)
+					cout << "  x    ";
+				else cout << "      ";
+			}
+			cout << endl;
+		}
+	}// R.2
+	
 	void Show_Staff_Requirements() {
 
 	}
